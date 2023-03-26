8ef206898a3b3e526d5341e39bfe5c868348e661
Muhammad Fathurrohim <m.faturrohim13@gmail.com>
2023-03-26 23:37:05 +0800

    feat(app): show product in landing page

--- app/src/Landing.tsx
+++ app/src/Landing.tsx
@@ -3,13 +3,58 @@ import React from "react";
 import { Link } from "react-router-dom";
 const AppBar = React.lazy(() => import("./AppBar"));
 import { useUser } from "./hooks/useUser";
+import useSWR from "swr";
+import { GetProduct, Product } from "./models/Product";
+import Card from "@mui/material/Card";
+import CardActionArea from "@mui/material/CardActionArea";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
 
 export default function Landing() {
   const user = useUser();
+  console.log(user.user);
+
+  const { data: productSwr } = useSWR<{ data: GetProduct }>(
+    "/api/v1/product",
+    (url) => axios.get(url)
+  );
+
+  const products = React.useMemo(
+    () => productSwr?.data?.products.map((it) => it).reverse(),
+    [productSwr?.data]
+  );
 
   return (
     <div className="App">
       <AppBar />
+
+      <Grid container spacing={4}>
+        {products?.map((it) => (
+          <Grid item key={it.id} xs={2}>
+            <ProductCard product={it} />
+          </Grid>
+        ))}
+      </Grid>
     </div>
   );
+}
+
+function ProductCard({ product }: { product: Product }) {
+  const navigate = useNavigate();
+  return (
+    <Card>
+      <CardActionArea onClick={() => navigate(`/product/${product.id}`)}>
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {product.name}
+          </Typography>
+          <Typography variant="body2" fontSize={15}>
+            Rp. {product.price}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+  //
 }
