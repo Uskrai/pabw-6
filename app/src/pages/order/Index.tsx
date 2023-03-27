a30504ab504f53ee3c206c4db1535b1fcc5abad8
Muhammad Fathurrohim <m.faturrohim13@gmail.com>
2023-03-28 07:09:26 +0800

    feat(app): improve order/index

--- app/src/pages/order/Index.tsx
+++ app/src/pages/order/Index.tsx
@@ -5,11 +5,12 @@ import Divider from "@mui/material/Divider";
 import CardContent from "@mui/material/CardContent";
 import CircularProgress from "@mui/material/CircularProgress";
 import Typography from "@mui/material/Typography";
-import { Link, Outlet, useNavigate } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import AppBar from "../../AppBar";
 import { useAuthSWR } from "../../hooks/useSWR";
 import { GetOrder, Transaction } from "../../models/Transaction";
 import { User } from "../../models/User";
+import { Product } from "../../models/Product";
 
 export default function ShowProduct() {
   let { data, isLoading } = useAuthSWR<GetOrder>(`/api/v1/order`);
@@ -41,8 +42,12 @@ export default function ShowProduct() {
 
 function OrderCard({ order }: { order: Transaction }) {
   const navigate = useNavigate();
-  let { data: merchant, isLoading } = useAuthSWR<User>(
+  let { data: merchant } = useAuthSWR<User>(
     `/api/v1/account/${order.merchant_id}`
+  );
+
+  const { data: product } = useAuthSWR<Product>(
+    `/api/v1/product/${order.products[0].id}`
   );
 
   return (
@@ -50,15 +55,17 @@ function OrderCard({ order }: { order: Transaction }) {
       <CardActionArea onClick={() => navigate(`/user/order/${order.id}`)}>
         <CardContent>
           <Typography gutterBottom variant="h5" component="div">
-            {order.id}
+            {product ? product.name : <CircularProgress />}
+            {order.products.length > 1 && (
+              <div>+{order.products.length - 1} Produk lainnya</div>
+            )}
           </Typography>
           <Typography variant="body2" color="text.secondary">
             {merchant == null ? <CircularProgress /> : <>{merchant.email}</>}
           </Typography>
-          {/* <Typography variant="body2" fontSize={12}> */}
-          {/*   Rp. {product.price} */}
-          {/* </Typography> */}
-          {/* <Typography variant="overline">{product.stock}</Typography> */}
+          <Typography variant="body2" color="text.secondary">
+            {order.status.type}
+          </Typography>
         </CardContent>
       </CardActionArea>
     </Card>
--- app/src/pages/order/Show.tsx
+++ app/src/pages/order/Show.tsx
@@ -1,57 +1,46 @@
 import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import CircularProgress from "@mui/material/CircularProgress";
 import Typography from "@mui/material/Typography";
 import axios from "axios";
 import React from "react";
-import { Link, useNavigate, useParams } from "react-router-dom";
-import useSWR, { mutate } from "swr";
-import { useAuth } from "../../hooks/useAuth";
+import { useParams } from "react-router-dom";
+import useSWR from "swr";
 import { useAuthSWR } from "../../hooks/useSWR";
 import { Product } from "../../models/Product";
 import { Transaction } from "../../models/Transaction";
+import { User } from "../../models/User";
 
 export default function ShowProduct() {
   let { id } = useParams();
 
-  let { data, isLoading } = useAuthSWR<Transaction>(`/api/v1/order/${id}`);
+  let { data: order, isLoading } = useAuthSWR<Transaction>(`/api/v1/order/${id}`);
+  let { data: merchant } = useAuthSWR<User>(order ? `/api/v1/account/${order.merchant_id}` : null)
 
-  const navigate = useNavigate();
-  const { token } = useAuth();
-
-  if (isLoading && data == null) {
+  if (isLoading && order == null) {
     return <CircularProgress />;
   }
 
-  let onDelete = async () => {
-    await axios.delete(`/api/v1/product/${id}`, {
-      headers: { Authorization: `Bearer ${token}` },
-    });
-    mutate("/api/v1/product");
-    navigate("/user/product");
-  };
-
   return (
     <Card sx={{ m: 2 }}>
-      {data ? (
+      {order ? (
         <>
           <CardContent>
             <Typography gutterBottom variant="h5" component="div">
-              Nama: {data.id}
+              Nama: {order.id}
             </Typography>
             <Typography variant="body2" color="text.secondary">
-              Merchant: {data.merchant_id}
+              Merchant: {merchant?.email}
             </Typography>
             <Typography variant="body2" fontSize={12}>
-              Harga: Rp. {data.price}
+              Harga: Rp. {order.price}
             </Typography>
             <Typography variant="body2" fontSize={12}>
-              Status: {data.status.type}
+              Status: {order.status.type}
             </Typography>
 
-            <Typography variant="h5">Product:</Typography>
-            {data.products.map((it) => {
+            <Typography variant="h5">Barang:</Typography>
+            {order.products.map((it) => {
               return (
                 <React.Fragment key={it.id}>
                   <ProductCard product={it} />
@@ -59,10 +48,6 @@ export default function ShowProduct() {
               );
             })}
           </CardContent>
-          {/* <CardActions> */}
-          {/*   <Link to={`/user/product/${product.id}/edit`}>Edit</Link> */}
-          {/*   <Link to={`/user/product/${product.id}`} onClick={onDelete}>Delete</Link> */}
-          {/* </CardActions> */}
         </>
       ) : null}
     </Card>
